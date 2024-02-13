9119b595ac4e6eff36143aadd4c67a8efe6a28d6
ehsan <ehsan@raychat.io>
2024-02-14 02:02:42 +0330

    update login/signup validator

--- src/modules/auth/interface/validate.joi.ts
+++ src/modules/auth/interface/validate.joi.ts
@@ -1,7 +1,8 @@
 import joi from 'joi';
 
 export const login = joi.object({
-
+  email: joi.string().email().max(200).required(),
+  password: joi.string().trim().min(8).max(45).required(),
 });
 
 export const logout = joi.object({
@@ -9,7 +10,9 @@ export const logout = joi.object({
 });
 
 export const signup = joi.object({
-
+  email: joi.string().email().max(200).required(),
+  username: joi.string().trim().max(45).required(),
+  password: joi.string().trim().min(8).max(45).required(),
 });
 
 export const refreshToken = joi.object({
