15e8f00c058f368ca09adc17dd816f90b11c59c8
ehsan <ehsan@raychat.io>
2024-02-14 02:01:28 +0330

    remove not usage code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import 'dotenv/config';
 import Connection from './helpers/connection';
-
 const ConnectionService = new Connection(express);
 
 ConnectionService.startup();
