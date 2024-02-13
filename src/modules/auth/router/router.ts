8c1229403a5beb41f95baf4495250138ec2fd29c
ehsan <ehsan@raychat.io>
2024-02-14 03:13:08 +0330

    update refreshtoken router

--- src/modules/auth/router/router.ts
+++ src/modules/auth/router/router.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import Auth from './../controller/auth.controller';
 import Validator from './../../../helpers/validator';
-import { login, logout, signup, refreshToken } from './../interface/validate.joi';
-
+import { login, logout, signup } from './../interface/validate.joi';
+import Authorization from '../../../helpers/authorization';
 const validate = new Validator('auth_service');
 const router = Router();
 
@@ -10,6 +10,9 @@ const AuthController = new Auth();
 router.post('/login', validate.validate(login, 'POST'), AuthController.login);
 router.post('/login', validate.validate(logout, 'POST'), AuthController.logout);
 router.post('/signup', validate.validate(signup, 'POST'), AuthController.signup);
-router.post('/refresh', validate.validate(refreshToken, 'POST'), AuthController.refreshToken);
+router.post('/refresh',
+  Authorization.refreshToken,
+  AuthController.refreshToken
+);
 
 export default router;
