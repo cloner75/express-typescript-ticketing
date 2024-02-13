7ac3b5f98db125659ff53e6a26eaedfad591ce87
ehsan <ehsan@raychat.io>
2024-02-14 02:03:48 +0330

    implement signupUser in userService

--- src/modules/user/utils/user.service.ts
+++ src/modules/user/utils/user.service.ts
@@ -1,7 +1,41 @@
 import UserModel from './../schema/user.schema';
+import * as bcrypt from 'bcrypt';
 
 class UserService {
-  async getUserByEmail(email: string) {
+  async signupUser(email: string, password: string, username: string): Promise<any> {
+
+    const getUser = await UserModel.exists({
+      $or: [
+        { email: email.toLowerCase() },
+        { username: username.toLowerCase() }
+      ]
+    });
+    if (getUser) {
+      return {
+        success: false,
+        message: 'email or username already exsits'
+      };
+    }
+
+    const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
+    const hash = await bcrypt.hash(password, salt);
+
+    const createUser = await UserModel.create({
+      username: username.toLowerCase(),
+      email: email.toLowerCase(),
+      password: hash,
+      salt,
+      role: 'admin',
+    });
+
+    return {
+      success: true,
+      message: 'create new user',
+      data: createUser
+    };
+  }
+
+  async getUserByEmail(email: string): Promise<any> {
     const getUser = await UserModel.findOne({ email });
     if (!getUser) {
       return {
