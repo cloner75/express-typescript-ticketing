8fe80fcd4a5017c50f991eb5ef54beaf9b43c871
ehsan <ehsan@raychat.io>
2024-02-14 02:03:17 +0330

    add salt to database for test

--- src/modules/user/schema/user.schema.ts
+++ src/modules/user/schema/user.schema.ts
@@ -5,6 +5,7 @@ import { USER_ROLES } from './../interface/role.interface';
 const UserSchema = new mongoose.Schema({
   username: { required: true, type: String },
   password: { required: true, type: String },
+  salt: { required: true, type: String },
   phone: { required: false, type: String },
   email: { required: false, type: String },
   role: { required: true, type: String, enum: USER_ROLES },
