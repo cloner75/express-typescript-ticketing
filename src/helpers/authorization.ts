9747d46331c17d541d9d0e052f7027fe35bde1e1
ehsan <ehsan@raychat.io>
2024-02-16 01:54:48 +0330

    update authorization service

--- src/helpers/authorization.ts
+++ src/helpers/authorization.ts
@@ -3,8 +3,19 @@ import cookie from 'cookie';
 import jsonwebtoken from './jsonwebtoken';
 import Responser from './response';
 import httpStatus from 'http-status';
+declare global {
+  namespace Express {
+    interface Request {
+      user: any,
+      query: any,
+      body: any,
+      params: any;
+    }
+  }
+}
 
 class Authorization {
+
   static authorization(req: Request, res: Response, next: any) {
     const responser = new Responser(res, 'authorization');
     try {
@@ -17,8 +28,7 @@ class Authorization {
         );
       }
       const verifyToken = jsonwebtoken.verify(access_token);
-      console.log("🚀 ~ Authorization ~ authorization ~ verifyToken:", verifyToken);
-      Object.assign(req.body, { user: verifyToken });
+      Object.assign(req, { user: verifyToken });
       next();
     } catch (err) {
       return responser.success(
