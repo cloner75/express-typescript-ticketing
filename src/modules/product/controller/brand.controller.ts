bf50b58243dfeeff254c3633e59f55e00d979349
ehsan <ehsan@raychat.io>
2024-02-16 03:29:26 +0330

    implement brand conroller

--- src/modules/product/controller/brand.controller.ts
+++ src/modules/product/controller/brand.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
-import UserService from '../../user/utils/user.service';
+import BrandService from '../utils/brand.service';
+
 import Responser from '../../../helpers/response';
 import httpStatus from 'http-status';
 
-class BrandController extends UserService {
+
+class BrandController extends BrandService {
   constructor() {
     super();
   }
+
   /**
    * 
    * @param req 
@@ -14,14 +17,23 @@ class BrandController extends UserService {
    * @returns 
    */
   async create(req: Request, res: Response): Promise<any> {
-    const sendResponse = new Responser(res, 'login');
+    const sendResponse = new Responser(res, 'brand-create');
     try {
-      const { email } = req.body;
+      const { email, _id: creator } = req.user;
       const getUser = await super.getUserByEmail(email);
       if (!getUser.success) {
-        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE']);
+        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE'], {
+          message: 'user not found'
+        });
       }
-      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getUser.data);
+
+      const getNewBrand = await super.createBrand(req.body, creator);
+      if (!getNewBrand.success) {
+        return sendResponse.success(false, httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], {
+          message: getNewBrand.message
+        });
+      }
+      return sendResponse.success(true, httpStatus.CREATED, httpStatus['201_MESSAGE'], getNewBrand.data);
     } catch (err) {
       return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
@@ -34,34 +46,32 @@ class BrandController extends UserService {
  * @returns 
  */
   async find(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'brand-find');
     try {
-      const { email, password } = req.body;
-      return res.send({
-        success: false
-      });
+      const getBrands = await super.getBrand(req.query);
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getBrands.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
 
   /**
-* 
-* @param req 
-* @param res 
-* @returns 
-*/
+  * 
+  * @param req 
+  * @param res 
+  * @returns 
+  */
   async findOne(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'brand-findOne');
     try {
-      return res.send({
-        success: false
-      });
+      const getBrand = await super.getBrandById(req.params.id);
+      if (!getBrand.success) {
+        return sendResponse.success(false, httpStatus.NO_CONTENT, httpStatus['204_MESSAGE'], getBrand.message);
+      }
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getBrand.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
@@ -72,14 +82,25 @@ class BrandController extends UserService {
 * @returns 
 */
   async update(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'brand-create');
     try {
-      return res.send({
-        success: false
-      });
+      const { email, _id: creator } = req.user;
+      const getUser = await super.getUserByEmail(email);
+      if (!getUser.success) {
+        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE'], {
+          message: 'user not found'
+        });
+      }
+
+      const getNewBrand = await super.updateBrandById(req.params.id, req.body, creator);
+      if (!getNewBrand.success) {
+        return sendResponse.success(false, httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], {
+          message: getNewBrand.message
+        });
+      }
+      return sendResponse.success(true, httpStatus.CREATED, httpStatus['201_MESSAGE'], getNewBrand.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
@@ -91,14 +112,15 @@ class BrandController extends UserService {
 * @returns 
 */
   async delete(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'brand-delete');
     try {
-      return res.send({
-        success: false
-      });
+      const removeBrand = await super.removeBrandById(req.params.id);
+      if (!removeBrand.success) {
+        return sendResponse.success(false, httpStatus.NO_CONTENT, httpStatus['204_MESSAGE'], removeBrand.message);
+      }
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], removeBrand.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 }
