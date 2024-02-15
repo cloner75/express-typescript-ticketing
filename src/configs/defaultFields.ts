fc3ece7940c7b2271710b9c395c36f56278d6b67
ehsan <ehsan@raychat.io>
2024-02-16 01:54:33 +0330

    implement defautl fields for projection

--- src/configs/defaultFields.ts
+++ src/configs/defaultFields.ts
@@ -1,16 +1,72 @@
-export default {
-  product: [],
-  brand: [],
-  category: [],
-  user: [],
-  customer: [],
-  agent: [],
-  invoice: [],
-  ticket: [],
-  request: [],
-  comment: [],
-  blog: [],
-  option: [],
-  wallet: [],
-  contactUs: [],
-};
+export default [
+  {
+    name: 'product',
+    value: [
+      '_id',
+      'name',
+      'title',
+      'description',
+      'slug',
+      'colers',
+      'images',
+      'options',
+      'brandId',
+      'categoryId',
+      'creator',
+      'createdAt',
+      'updatedAt',
+    ],
+  },
+  {
+    name: 'brand',
+    value: [],
+  },
+  {
+    name: 'category',
+    value: [],
+  },
+  {
+    name: 'user',
+    value: [],
+  },
+  {
+    name: 'customer',
+    value: [],
+  },
+  {
+    name: 'agent',
+    value: [],
+  },
+  {
+    name: 'invoice',
+    value: [],
+  },
+  {
+    name: 'ticket',
+    value: [],
+  },
+  {
+    name: 'request',
+    value: [],
+  },
+  {
+    name: 'comment',
+    value: [],
+  },
+  {
+    name: 'blog',
+    value: [],
+  },
+  {
+    name: 'option',
+    value: [],
+  },
+  {
+    name: 'wallet',
+    value: [],
+  },
+  {
+    name: 'contactUs',
+    value: [],
+  },
+];
