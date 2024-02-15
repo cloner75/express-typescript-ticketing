0e11698c9dffdff1f317f74cf907abe187d4ed1b
ehsan <ehsan@raychat.io>
2024-02-16 03:29:51 +0330

    implement product controller

--- src/modules/product/controller/product.controller.ts
+++ src/modules/product/controller/product.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
-import UserService from '../../user/utils/user.service';
+import ProductService from '../utils/product.service';
+
 import Responser from '../../../helpers/response';
 import httpStatus from 'http-status';
 
-class ProductController extends UserService {
+
+class ProductController extends ProductService {
   constructor() {
     super();
   }
+
   /**
    * 
    * @param req 
@@ -14,14 +17,23 @@ class ProductController extends UserService {
    * @returns 
    */
   async create(req: Request, res: Response): Promise<any> {
-    const sendResponse = new Responser(res, 'login');
+    const sendResponse = new Responser(res, 'product-create');
     try {
-      const { email } = req.body;
+      const { email, _id: creator } = req.user;
       const getUser = await super.getUserByEmail(email);
       if (!getUser.success) {
-        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE']);
+        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE'], {
+          message: 'user not found'
+        });
       }
-      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getUser.data);
+
+      const getNewProduct = await super.createProduct(req.body, creator);
+      if (!getNewProduct.success) {
+        return sendResponse.success(false, httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], {
+          message: getNewProduct.message
+        });
+      }
+      return sendResponse.success(true, httpStatus.CREATED, httpStatus['201_MESSAGE'], getNewProduct.data);
     } catch (err) {
       return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
@@ -34,34 +46,32 @@ class ProductController extends UserService {
  * @returns 
  */
   async find(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'product-find');
     try {
-      const { email, password } = req.body;
-      return res.send({
-        success: false
-      });
+      const getProducts = await super.getProduct(req.query);
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getProducts.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
 
   /**
-* 
-* @param req 
-* @param res 
-* @returns 
-*/
+  * 
+  * @param req 
+  * @param res 
+  * @returns 
+  */
   async findOne(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'product-findOne');
     try {
-      return res.send({
-        success: false
-      });
+      const getProduct = await super.getProductById(req.params.id);
+      if (!getProduct.success) {
+        return sendResponse.success(false, httpStatus.NO_CONTENT, httpStatus['204_MESSAGE'], getProduct.message);
+      }
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getProduct.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
@@ -72,14 +82,25 @@ class ProductController extends UserService {
 * @returns 
 */
   async update(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'product-create');
     try {
-      return res.send({
-        success: false
-      });
+      const { email, _id: creator } = req.user;
+      const getUser = await super.getUserByEmail(email);
+      if (!getUser.success) {
+        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE'], {
+          message: 'user not found'
+        });
+      }
+
+      const getNewProduct = await super.updateProductById(req.params.id, req.body, creator);
+      if (!getNewProduct.success) {
+        return sendResponse.success(false, httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], {
+          message: getNewProduct.message
+        });
+      }
+      return sendResponse.success(true, httpStatus.CREATED, httpStatus['201_MESSAGE'], getNewProduct.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
@@ -91,14 +112,15 @@ class ProductController extends UserService {
 * @returns 
 */
   async delete(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'product-delete');
     try {
-      return res.send({
-        success: false
-      });
+      const removeProduct = await super.removeProductById(req.params.id);
+      if (!removeProduct.success) {
+        return sendResponse.success(false, httpStatus.NO_CONTENT, httpStatus['204_MESSAGE'], removeProduct.message);
+      }
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], removeProduct.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 }
