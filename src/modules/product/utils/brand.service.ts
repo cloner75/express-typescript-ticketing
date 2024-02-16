d8041f67eb27d4c0f7fa6c16d07f847f585a1ec4
ehsan <ehsan@raychat.io>
2024-02-16 03:30:25 +0330

    implement brand service

--- src/modules/product/utils/brand.service.ts
+++ src/modules/product/utils/brand.service.ts
@@ -1,32 +1,50 @@
 import BrandModel from './../schema/brand.schema';
 import MongoQuery from './../../../helpers/mongo.query';
 import responser from '../../../helpers/response';
+import UserService from '../../user/utils/user.service';
 
-class BrandService {
-  #MongoHelper: any;
+
+class BrandService extends UserService {
   constructor() {
-    this.#MongoHelper = MongoQuery;
+    super();
   }
 
+  /**
+   * 
+   * @param inputData u
+   * @param creator 
+   * @returns 
+   */
   async createBrand(inputData: any, creator: string): Promise<any> {
     try {
       const newBrand = await BrandModel.create({ ...inputData, creator });
       return responser.serviceResponse(true, 'isOK', newBrand);
-    } catch (err) {
+    } catch (err: any) {
       return responser.serviceResponse(false, 'can not create brand');
     }
   }
 
+  /**
+   * 
+   * @param inputWhere 
+   * @returns 
+   */
   async getBrand(inputWhere: any): Promise<any> {
     try {
-      const { options, where } = this.#MongoHelper.initialMongoQuery(inputWhere);
-      const getCategories = await BrandModel.find(where, options);
-      return responser.serviceResponse(true, 'isOK', getCategories);
+      const { options, where } = MongoQuery.initialMongoQuery(inputWhere, 'brand');
+      const { select: projection, ...otherOptions } = options;
+      const findByQuery = await BrandModel.find(where, projection, otherOptions);
+      return responser.serviceResponse(true, 'isOK', findByQuery);
     } catch (err) {
-      return responser.serviceResponse(false, 'can not get categories');
+      return responser.serviceResponse(false, 'can not get brands');
     }
   }
 
+  /**
+   * 
+   * @param id 
+   * @returns 
+   */
   async getBrandById(id: string) {
     try {
       const findOneBrand = await BrandModel.findOne({ _id: id });
@@ -39,9 +57,9 @@ class BrandService {
     }
   }
 
-  async updateBrandById(id: string, updatedFields: any) {
+  async updateBrandById(id: string, updatedFields: any, creator: string) {
     try {
-      const updateFields = await BrandModel.updateOne({ _id: id }, { $set: updatedFields });
+      const updateFields = await BrandModel.updateOne({ _id: id }, { $set: { ...updatedFields, creator } });
       if (!updateFields || !updateFields.modifiedCount || !updateFields.acknowledged) {
         return responser.serviceResponse(false, 'brand not found');
       }
@@ -56,7 +74,7 @@ class BrandService {
   async removeBrandById(id: string) {
     try {
       const updateFields = await BrandModel.deleteOne({ _id: id });
-      if (!updateFields || !updateFields.acknowledged) {
+      if (!updateFields || !updateFields.acknowledged || !updateFields.deletedCount) {
         return responser.serviceResponse(false, 'brand not found');
       }
       return responser.serviceResponse(true, 'isOK', updateFields);
