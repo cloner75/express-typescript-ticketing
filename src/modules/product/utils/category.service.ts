95637e57ddfd7def3756db674e842ec89e935531
ehsan <ehsan@raychat.io>
2024-02-16 03:30:36 +0330

    implement category service

--- src/modules/product/utils/category.service.ts
+++ src/modules/product/utils/category.service.ts
@@ -1,32 +1,50 @@
 import CategoryModel from './../schema/category.schema';
 import MongoQuery from './../../../helpers/mongo.query';
 import responser from '../../../helpers/response';
+import UserService from '../../user/utils/user.service';
 
-class CategoryService {
-  #MongoHelper: any;
+
+class CategoryService extends UserService {
   constructor() {
-    this.#MongoHelper = MongoQuery;
+    super();
   }
 
+  /**
+   * 
+   * @param inputData u
+   * @param creator 
+   * @returns 
+   */
   async createCategory(inputData: any, creator: string): Promise<any> {
     try {
       const newCategory = await CategoryModel.create({ ...inputData, creator });
       return responser.serviceResponse(true, 'isOK', newCategory);
-    } catch (err) {
+    } catch (err: any) {
       return responser.serviceResponse(false, 'can not create category');
     }
   }
 
+  /**
+   * 
+   * @param inputWhere 
+   * @returns 
+   */
   async getCategory(inputWhere: any): Promise<any> {
     try {
-      const { options, where } = this.#MongoHelper.initialMongoQuery(inputWhere);
-      const getCategories = await CategoryModel.find(where, options);
-      return responser.serviceResponse(true, 'isOK', getCategories);
+      const { options, where } = MongoQuery.initialMongoQuery(inputWhere, 'category');
+      const { select: projection, ...otherOptions } = options;
+      const findByQuery = await CategoryModel.find(where, projection, { ...otherOptions });
+      return responser.serviceResponse(true, 'isOK', findByQuery);
     } catch (err) {
-      return responser.serviceResponse(false, 'can not get categories');
+      return responser.serviceResponse(false, 'can not get categorys');
     }
   }
 
+  /**
+   * 
+   * @param id 
+   * @returns 
+   */
   async getCategoryById(id: string) {
     try {
       const findOneCategory = await CategoryModel.findOne({ _id: id });
@@ -39,9 +57,9 @@ class CategoryService {
     }
   }
 
-  async updateCategoryById(id: string, updatedFields: any) {
+  async updateCategoryById(id: string, updatedFields: any, creator: string) {
     try {
-      const updateFields = await CategoryModel.updateOne({ _id: id }, { $set: updatedFields });
+      const updateFields = await CategoryModel.updateOne({ _id: id }, { $set: { ...updatedFields, creator } });
       if (!updateFields || !updateFields.modifiedCount || !updateFields.acknowledged) {
         return responser.serviceResponse(false, 'category not found');
       }
@@ -56,7 +74,7 @@ class CategoryService {
   async removeCategoryById(id: string) {
     try {
       const updateFields = await CategoryModel.deleteOne({ _id: id });
-      if (!updateFields || !updateFields.acknowledged) {
+      if (!updateFields || !updateFields.acknowledged || !updateFields.deletedCount) {
         return responser.serviceResponse(false, 'category not found');
       }
       return responser.serviceResponse(true, 'isOK', updateFields);
