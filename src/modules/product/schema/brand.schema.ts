e5e04ab4a34bbdab810cefeac87eeb04ea152233
ehsan <ehsan@raychat.io>
2024-02-16 03:30:03 +0330

    update brand schema mongoose

--- src/modules/product/schema/brand.schema.ts
+++ src/modules/product/schema/brand.schema.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 import paginate from 'mongoose-paginate-v2';
 
-const CategorySchema = new mongoose.Schema({
-  name: { type: String, required: true }
+const BrandSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  creator: { type: mongoose.Schema.Types.ObjectId, required: true }
 });
 
-CategorySchema.plugin(paginate);
-CategorySchema.plugin(require('mongoose-timestamp'));
+BrandSchema.plugin(paginate);
+BrandSchema.plugin(require('mongoose-timestamp'));
 
-const BrandModel = mongoose.model('brands', CategorySchema);
+const BrandModel = mongoose.model('brands', BrandSchema);
 
 export default BrandModel;
