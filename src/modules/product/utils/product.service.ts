ec50240e27d1cf05cfa0cea8e1ffaf945b327b3a
ehsan <ehsan@raychat.io>
2024-02-16 03:30:47 +0330

    implement product service

--- src/modules/product/utils/product.service.ts
+++ src/modules/product/utils/product.service.ts
@@ -1,43 +1,87 @@
 import ProductModel from './../schema/product.schema';
 import MongoQuery from './../../../helpers/mongo.query';
+import responser from '../../../helpers/response';
+import UserService from '../../user/utils/user.service';
 
-class ProductService {
-  #MongoHelper: any;
+
+class ProductService extends UserService {
   constructor() {
-    this.#MongoHelper = MongoQuery;
+    super();
   }
-  async createProduct() {
+
+  /**
+   * 
+   * @param inputData u
+   * @param creator 
+   * @returns 
+   */
+  async createProduct(inputData: any, creator: string): Promise<any> {
     try {
-
-    } catch (err) {
-
+      const newProduct = await ProductModel.create({ ...inputData, creator });
+      return responser.serviceResponse(true, 'isOK', newProduct);
+    } catch (err: any) {
+      return responser.serviceResponse(false, 'can not create product');
     }
   }
 
-  async getProduct() {
+  /**
+   * 
+   * @param inputWhere 
+   * @returns 
+   */
+  async getProduct(inputWhere: any): Promise<any> {
     try {
-
+      const { options, where } = MongoQuery.initialMongoQuery(inputWhere, 'product');
+      const { select: projection, ...otherOptions } = options;
+      const findByQuery = await ProductModel.find(where, projection, { ...otherOptions });
+      return responser.serviceResponse(true, 'isOK', findByQuery);
     } catch (err) {
-
+      return responser.serviceResponse(false, 'can not get products');
     }
   }
 
-  async getProductById() {
+  /**
+   * 
+   * @param id 
+   * @returns 
+   */
+  async getProductById(id: string) {
     try {
-
+      const findOneProduct = await ProductModel.findOne({ _id: id });
+      if (!findOneProduct) {
+        return responser.serviceResponse(false, 'product not found');
+      }
+      return responser.serviceResponse(true, 'isOK', findOneProduct);
     } catch (err) {
-
+      return responser.serviceResponse(false, 'can not get product');
     }
   }
 
-  async updateProductById() {
+  async updateProductById(id: string, updatedFields: any, creator: string) {
     try {
-
+      const updateFields = await ProductModel.updateOne({ _id: id }, { $set: { ...updatedFields, creator } });
+      if (!updateFields || !updateFields.modifiedCount || !updateFields.acknowledged) {
+        return responser.serviceResponse(false, 'product not found');
+      }
+      return responser.serviceResponse(true, 'isOK', updateFields);
     } catch (err) {
-
+      return responser.serviceResponse(false, 'can not update product');
     }
   }
 
+
+
+  async removeProductById(id: string) {
+    try {
+      const updateFields = await ProductModel.deleteOne({ _id: id });
+      if (!updateFields || !updateFields.acknowledged || !updateFields.deletedCount) {
+        return responser.serviceResponse(false, 'product not found');
+      }
+      return responser.serviceResponse(true, 'isOK', updateFields);
+    } catch (err) {
+      return responser.serviceResponse(false, 'can not remove product');
+    }
+  }
 }
 
 export default ProductService;
