4fe4d965d786bb3f4a3143c640bbc89172817d41
ehsan <ehsan@raychat.io>
2024-02-19 02:20:50 +0330

    implement user service all validations

--- src/modules/user/interface/validate.joi.ts
+++ src/modules/user/interface/validate.joi.ts
@@ -1,22 +1,127 @@
-import joi from 'joi';
+import joi, { required } from 'joi';
 
-export const create = joi.object({
+const id: any = {
+  required: joi
+    .string()
+    .trim()
+    .pattern(/^[0-9a-fA-F]{24}$/)
+    .required(),
+  unnecessary: joi
+    .string()
+    .trim()
+    .pattern(/^[0-9a-fA-F]{24}$/)
+};
 
-});
+const isPersianText = {
+  required: joi
+    .string()
+    .trim()
+    .pattern(/([-.]*\\s*[-.]*\\p{Arabic}*[-.]*\\s*)*[-.]*/)
+    .required(),
+  unnecessary: joi
+    .string()
+    .trim()
+    .pattern(/([-.]*\\s*[-.]*\\p{Arabic}*[-.]*\\s*)*[-.]*/)
+};
 
-export const find = joi.object({
+export const user = {
+  create: joi.object({
 
-});
+  }),
 
-export const findOne = joi.object({
+  find: joi.object({
 
-});
+  }),
 
-export const update = joi.object({
+  findOne: joi.object({
 
-});
+  }),
 
-export const remove = joi.object({
+  update: joi.object({
 
-});
+  }),
+
+  remove: joi.object({
+
+  }),
+
+};
+
+
+export const role = {
+  create: joi.object({
+    name: joi.string().trim().required(),
+    nameFa: isPersianText.required,
+  }),
+
+  find: joi.object({
+    name: joi.string().trim(),
+    nameFa: isPersianText.unnecessary,
+    fields: joi.string().trim(),
+    limit: joi.number(),
+    skip: joi.number(),
+    sort: joi.number().valid(-1, 1),
+    order: joi.string().trim().valid('_id', 'createdAt', 'updatedAt'),
+    page: joi.number(),
+    ids: joi.string().trim()
+  }),
+
+  findOne: joi.object({
+    id: id.required,
+    fields: joi.string().trim(),
+  }),
+
+  update: joi.object({
+    id: id.required,
+    name: joi.string().trim().required(),
+    nameFa: isPersianText.required,
+  }),
+
+  remove: joi.object({
+    id: id.required,
+  }),
+
+};
+
+
+export const permission = {
+
+  create: joi.object({
+    roleId: id.required,
+    access: joi.array().items(joi.object({
+      service: joi.string().trim().required(),
+      methods: joi.array().items(joi.string().trim().required()).required(),
+    })).required(),
+  }),
+
+  find: joi.object({
+    fields: joi.string().trim(),
+    limit: joi.number(),
+    skip: joi.number(),
+    sort: joi.number().valid(-1, 1),
+    order: joi.string().trim().valid('_id', 'createdAt', 'updatedAt'),
+    page: joi.number(),
+    ids: joi.string().trim()
+  }),
+
+  findOne: joi.object({
+    id: id.required,
+    fields: joi.string().trim(),
+  }),
+
+  update: joi.object({
+    id: id.required,
+    roleId: id.required,
+    access: joi.array().items(joi.object({
+      service: joi.string().trim().required(),
+      methods: joi.array().items(joi.string().trim().required()).required(),
+    })).required(),
+  }),
+
+  remove: joi.object({
+    id: id.required
+  }),
+
+};
+
 
