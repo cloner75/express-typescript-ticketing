e4d8dd7b0b38c778d445e4313e07cc71f67b8f8f
ehsan <ehsan@raychat.io>
2024-02-19 02:21:19 +0330

    ::init:: permission schema

---
+++ src/modules/user/schema/permission.schema.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import paginate from 'mongoose-paginate-v2';
+
+const PermissionSchema = new mongoose.Schema({
+  roleId: { required: true, type: String },
+  access: [
+    {
+      service: { required: true, type: String },
+      methods: { required: true, type: Array }
+    }
+  ],
+  creator: { required: false, type: mongoose.Schema.Types.ObjectId },
+});
+
+PermissionSchema.plugin(paginate);
+PermissionSchema.plugin(require('mongoose-timestamp'));
+
+const PermissionModel = mongoose.model('permissions', PermissionSchema);
+
+export default PermissionModel;
