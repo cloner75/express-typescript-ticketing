76dbd14a63e50be95a4964f9535dd4bc0ccbcd9a
ehsan <ehsan@raychat.io>
2024-02-19 02:19:39 +0330

    add role , permission to app module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,6 +1,17 @@
-import routes from './router/user.router';
+import userRoutes from './router/user.router';
+import roleRoutes from './router/role.router';
+import permissionRoutes from './router/permission.router';
 
-export default {
+export const user = {
   prefix: '/user',
-  routes
+  routes: userRoutes
+};
+
+export const role = {
+  prefix: '/role',
+  routes: roleRoutes
+};
+export const permission = {
+  prefix: '/permission',
+  routes: permissionRoutes
 };
