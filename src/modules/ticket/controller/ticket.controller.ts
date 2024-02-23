35ee006b9d630b5e491369359e965f30b8cc9498
ehsan <ehsan@raychat.io>
2024-02-23 12:15:46 +0330

    update ticket controller

--- src/modules/ticket/controller/ticket.controller.ts
+++ src/modules/ticket/controller/ticket.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
-import UserService from '../../user/utils/user.service';
+import TicketService from '../utils/ticket.service';
+
 import Responser from '../../../helpers/response';
 import httpStatus from 'http-status';
 
-class TicketController extends UserService {
+
+class TicketController extends TicketService {
   constructor() {
     super();
   }
+
   /**
    * 
    * @param req 
@@ -14,14 +17,23 @@ class TicketController extends UserService {
    * @returns 
    */
   async create(req: Request, res: Response): Promise<any> {
-    const sendResponse = new Responser(res, 'login');
+    const sendResponse = new Responser(res, 'ticket-create');
     try {
-      const { email } = req.body;
+      const { email, _id: creator } = req.user;
       const getUser = await super.getUserByEmail(email);
       if (!getUser.success) {
-        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE']);
+        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE'], {
+          message: 'user not found'
+        });
       }
-      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getUser.data);
+
+      const getNewTicket = await super.createTicket(req.body, creator);
+      if (!getNewTicket.success) {
+        return sendResponse.success(false, httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], {
+          message: getNewTicket.message
+        });
+      }
+      return sendResponse.success(true, httpStatus.CREATED, httpStatus['201_MESSAGE'], getNewTicket.data);
     } catch (err) {
       return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
@@ -34,34 +46,32 @@ class TicketController extends UserService {
  * @returns 
  */
   async find(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'ticket-find');
     try {
-      const { email, password } = req.body;
-      return res.send({
-        success: false
-      });
+      const getTickets = await super.getTicket(req.query);
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getTickets.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
 
   /**
-* 
-* @param req 
-* @param res 
-* @returns 
-*/
+  * 
+  * @param req 
+  * @param res 
+  * @returns 
+  */
   async findOne(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'ticket-findOne');
     try {
-      return res.send({
-        success: false
-      });
+      const getTicket = await super.getTicketById(req.params.id);
+      if (!getTicket.success) {
+        return sendResponse.success(false, httpStatus.NO_CONTENT, httpStatus['204_MESSAGE'], getTicket.message);
+      }
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], getTicket.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
@@ -72,14 +82,25 @@ class TicketController extends UserService {
 * @returns 
 */
   async update(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'ticket-create');
     try {
-      return res.send({
-        success: false
-      });
+      const { email, _id: creator } = req.user;
+      const getUser = await super.getUserByEmail(email);
+      if (!getUser.success) {
+        return sendResponse.success(false, httpStatus.OK, httpStatus['204_MESSAGE'], {
+          message: 'user not found'
+        });
+      }
+
+      const getNewTicket = await super.updateTicketById(req.params.id, req.body, creator);
+      if (!getNewTicket.success) {
+        return sendResponse.success(false, httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], {
+          message: getNewTicket.message
+        });
+      }
+      return sendResponse.success(true, httpStatus.CREATED, httpStatus['201_MESSAGE'], getNewTicket.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 
@@ -91,14 +112,15 @@ class TicketController extends UserService {
 * @returns 
 */
   async delete(req: Request, res: Response): Promise<any> {
+    const sendResponse = new Responser(res, 'ticket-delete');
     try {
-      return res.send({
-        success: false
-      });
+      const removeTicket = await super.removeTicketById(req.params.id);
+      if (!removeTicket.success) {
+        return sendResponse.success(false, httpStatus.NO_CONTENT, httpStatus['204_MESSAGE'], removeTicket.message);
+      }
+      return sendResponse.success(true, httpStatus.OK, httpStatus['200_MESSAGE'], removeTicket.data);
     } catch (err) {
-      return res.send({
-        err: 'err.message'
-      });
+      return sendResponse.error(httpStatus.INTERNAL_SERVER_ERROR, httpStatus['500_MESSAGE'], err);
     }
   }
 }
