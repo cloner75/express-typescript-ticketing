23aad1aa46e1754cf98b76f958a892b18ef91526
ehsan <ehsan@raychat.io>
2024-02-24 00:53:05 +0330

    implement permission service

---
+++ src/configs/permissions.ts
@@ -0,0 +1,119 @@
+export const product = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+export const category = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+export const brand = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+export const role = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+export const permission = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+export const agent = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const blog = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const comment = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const contactUs = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const customer = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const option = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const ticket = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const user = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
+
+
+export const wallet = {
+  create: 'create',
+  find: 'find',
+  findOne: 'findOne',
+  update: 'update',
+  delete: 'delete',
+};
